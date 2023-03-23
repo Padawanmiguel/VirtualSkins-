fa25b4bc9ef64ba56836e6cdc6392b022dff04be
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-03-22 22:50:50 -0300

    card swiper

--- swiper.js
+++ swiper.js
@@ -1,10 +1,14 @@
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 1,
   spaceBetween: 25,
-  loop: true,
   centerSlide: 'true',
   fade:  'true',
   grabCursor: 'true',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
