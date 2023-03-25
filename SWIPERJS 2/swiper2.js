ad3bf047885e5fa6aafca0903fa50fbb93886c73
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-03-24 21:18:24 -0300

    login and register

--- SWIPERJS 2/swiper2.js
+++ SWIPERJS 2/swiper2.js
@@ -1,17 +1,9 @@
 var swiper = new Swiper(".on-sale-content", {
   slidesPerView: 2,
   slidesPerGroup: 2,
-  spaceBetween: 60,
-  centerSlide: 'true',
-  fade:  'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: 'true',
-  },
+  
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-    },
+  },
 });
